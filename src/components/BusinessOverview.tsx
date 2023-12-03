f11b5bdf70d656d159868d87540f3a7a10c5d966
hiro0218 <hiro0218@gmail.com>
2023-12-03 14:33:54 +0900

    add: overview

---
+++ src/components/BusinessOverview.tsx
@@ -0,0 +1,51 @@
+export default function () {
+  return (
+    <section>
+      <h2>事業概要</h2>
+      <div>
+        <h3>
+          <span aria-hidden="true">■</span>
+          コンサルティング
+        </h3>
+        <p>
+          お客様の視点に立ったITコンサルティングをご提供いたします。
+          <br />
+          環境から、システム構築に関するプロジェクトマネジメントをご提供いたします。
+        </p>
+        <h3>
+          <span aria-hidden="true">■</span>技術支援、ソフトウェア開発
+        </h3>
+        <p>
+          設計常駐や一括での受託など、ご希望に沿った形式で承ります。
+          <br />
+          各種業務ソフト、Web開発システムをご提供いたします。
+        </p>
+        <h3>
+          <span aria-hidden="true">■</span>ネットワーク構築、サーバ設定
+        </h3>
+        <p>
+          ネットワークを元にサーバシステムの構築をご提供いたします。
+          <br />
+          LAN・WANにおける工事、構築、設定、運用、管理をご提供いたします。
+        </p>
+      </div>
+      <div>
+        <h3>利用技術</h3>
+        <h4>開発言語</h4>
+        <ul>
+          <li>PHP</li>
+          <li>C# .NET</li>
+          <li>Java</li>
+          <li>C++</li>
+          <li>C</li>
+        </ul>
+        <h4>データベース</h4>
+        <ul>
+          <li>SQL Server</li>
+          <li>Oracle</li>
+          <li>MySQL</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import { Head } from 'minista';
 
-import About from '../components/about';
+import 'shokika.css';
+
+import CompanyOverview from '../components/CompanyOverview';
+import BusinessOverview from '../components/BusinessOverview';
 
 export default function () {
   return (
     <>
       <Head>
-        <title>Hello!</title>
+        <title>株式会社KESソリューション</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -15,8 +18,16 @@ export default function () {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
-      <h1>Hello!</h1>
-      <About />
+      <header>
+        <h1>株式会社KESソリューション</h1>
+      </header>
+      <CompanyOverview />
+      <BusinessOverview />
+      <footer>
+        <p>
+          <small>&copy; 株式会社KESソリューション</small>
+        </p>
+      </footer>
     </>
   );
 }
