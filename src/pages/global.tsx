23fc5ec4727a585c7e24886fdb1ca6e50d041928
hiro0218 <hiro0218@gmail.com>
2023-12-03 14:46:32 +0900

    fix: global, style

---
+++ src/pages/global.tsx
@@ -0,0 +1,29 @@
+import type { GlobalProps } from 'minista';
+import { Head } from 'minista';
+
+import 'shokika.css';
+import '../../assets/style.css';
+
+export default function ({ url, title, children }: GlobalProps) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#da532c" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      {children}
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import { Head } from 'minista';
 
-import 'shokika.css';
-
 import CompanyOverview from '../components/CompanyOverview';
 import BusinessOverview from '../components/BusinessOverview';
 
@@ -10,13 +8,6 @@ export default function () {
     <>
       <Head>
         <title>株式会社KESソリューション</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#da532c" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
       </Head>
       <header>
         <h1>株式会社KESソリューション</h1>
