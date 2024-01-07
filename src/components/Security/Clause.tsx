710ca49aa14a4405b9111b9f04f5f4cbfbdf374b
hiro0218 <hiro0218@gmail.com>
2024-01-08 02:17:58 +0900

    Add styling to Clause component

--- src/components/Security/Clause.tsx
+++ src/components/Security/Clause.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { gray } from '@radix-ui/colors';
 
 const List = styled.ol`
   margin-top: 4rem;
@@ -13,11 +14,17 @@ const Item = styled.li`
   text-indent: -3ch;
 
   &::marker {
+    color: ${gray.gray12};
     font-weight: 700;
   }
 
   & + & {
     margin-top: 0.5em;
+  }
+
+  b {
+    color: ${gray.gray12};
+    font-weight: 700;
   }
 `;
 
