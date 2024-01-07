c41fe4cd4c26bd8b3ead6a042d7ed0cca83c1343
hiro0218 <hiro0218@gmail.com>
2024-01-07 22:23:46 +0900

    Add Page component styles

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,23 @@
+import styled from '@emotion/styled';
+
+export const PageHeading = styled.h2`
+  padding: 2rem 0;
+  font-weight: 900;
+`;
+
+export const LeadParagraph = styled.p`
+  letter-spacing: 0.04em;
+  line-height: 2;
+
+  & + & {
+    margin-top: 1em;
+  }
+`;
+
+export const PageSection = styled.section`
+  padding: 0 64px;
+
+  @media screen and (max-width: 767px) {
+    padding: 0 16px;
+  }
+`;
