1184cec29145a9b686c0f3e2336b71594b54a4b5
hiro0218 <hiro0218@gmail.com>
2024-01-07 22:21:55 +0900

    Refactor Footer component styling and add security link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,23 +1,34 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
 
+import { gray } from '@radix-ui/colors';
+
 export const Footer = () => {
   return (
     <Container>
-      <div>
-        <Link href="/security">情報セキュリティ基本方針</Link>
-      </div>
-      <p>
-        <small>&copy; 株式会社KESソリューション</small>
-      </p>
+      <TermLinkList>
+        <ul>
+          <li>
+            <Link href="/security">情報セキュリティ基本方針</Link>
+          </li>
+        </ul>
+      </TermLinkList>
+      <CompanyInfo>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src="logo.svg" alt="株式会社KESソリューション" width="60" />
+        <p>
+          <small>&copy; 株式会社KESソリューション</small>
+        </p>
+      </CompanyInfo>
     </Container>
   );
 };
 
 const Container = styled.footer`
-  padding-top: 64px;
-  padding-bottom: 64px;
+  padding: 64px;
   margin-top: 2rem;
+  text-align: center;
+  background-color: ${gray.gray2};
 
   a {
     font-size: 0.9rem;
@@ -26,4 +37,25 @@ const Container = styled.footer`
   small {
     font-size: 1rem;
   }
+`;
+
+const TermLinkList = styled.nav`
+  padding: 1.75rem 0;
+  font-weight: 700;
+  text-align: left;
+
+  a {
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const CompanyInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 2.5rem;
 `;
