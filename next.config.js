80f754af9c0bb0ca81d5bbc70030eb82818bb0cf
hiro0218 <hiro0218@gmail.com>
2024-01-07 23:05:49 +0900

    Update deployment configuration and next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   basePath: process.env.GITHUB_ACTIONS ? '/kes-solution' : '',
   compiler: {
     emotion: true,
